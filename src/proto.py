06cf3b431e9580592f66e28032d34ab650dc4a2f
gspindles <joseph.m.ching@gmail.com>
2014-02-24 23:38:28 -0600

    wrote some code in src/proto.py, mainly directory related code

--- src/proto.py
+++ src/proto.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python27
+
+### for now, this is a simple script to import all the yaml files and pretty print it
 
 import yaml
 import pprint
 import sys
+from os import listdir, chdir
+from os.path import isfile, dirname, join
 
-meeting = open("meeting.yaml", 'r')
-# print yaml.load(meeting)
-yaml.dump(yaml.load(meeting), sys.out)
+# change the current directory to the meetings directory where all the yaml files are located
+os.chdir('../meetings/')
+
+# get a list of all the yaml files
+yamlfiles = [f for f in os.listdir() if os.path.isfile(f) and ".yaml" in f]
+
+meeting = []
+for f in yaml:
+    m = open(f, 'r')
+
+    meeting.append(yaml.load(m))
+
+    # print yaml.load(meeting), used to have sys.out as second before to print to terminal
+    print yaml.dump(yaml.load(m))
