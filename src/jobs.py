e899edab68fc83223373b423f30523dfcf041821
Josh Tan <jtan189@gmail.com>
2014-03-07 18:04:52 -0600

    Implemented basic ical conversion. Some hardcoding involved, which will need to be removed.
    Improved ical description.
    
    Cleaned up code/yaml.
    
    Use uuid as ical filename.
    
    Remove __pycache__ directory.
    
    Implemented support for multiple events in a single ical file.
    
    Removed hardcoding of event frequency.
    
    Added comment about meeting duration.
    
    Modified yaml files by removing uuid field, renaming 'Period' to 'Schedule', and removing 'UTC' from the time field values. Relocated yaml to meetings/ directory. Also now use pprint.pformat to print agenda. This resulting output isn't very user-friendly, so it should be improved in the future.
    
    updated yaml files and made minor formatting changes
    
    removed libs and edited .gitignore
    
    Removed old yaml files.
    
    Made time a string in yaml.
    
    iCal files now use same filename as YAML files.
    
    Add event times as UTC.
    
    Changed agenda output in iCal description to a YAML dump of the agenda.
    
    updated yaml files some more
    
    When finding yaml files in MeetingJobs.create_meetings(), program now tests whether the filename ends with '.yaml', instead of if it contains '.yaml'. (This avoids the problem of temporary emacs files like '#meeting.yaml#'.)
    
    Added basic logging functionality.
    
    Change-Id: Ib6cbe5b54228595faa42ccdadc6be15ee4c278da

--- src/jobs.py
+++ src/jobs.py
@@ -3,19 +3,30 @@ import icalendar
 import pprint
 import sys
 import os
+import uuid
 from meeting import Meeting
+import logging
 
+# logging settings
+logging.basicConfig(format='%(asctime)s  - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+yaml_dir = '../meetings'
+ical_dir = '../icals'
+publish_url = '127.0.0.1'
+
+# should we make execute_gate(), etc. static methods instead?
 class MeetingJobs:
     """Executes post, gate, and check jobs."""
 
-    yaml_dir = '../meetings'
-    publish_url = '127.0.0.1'
-
     def execute_check(self):
-        meetings = self.create_meetings(self.yaml_dir)
-        meetings_display = "\n".join([m.display() for m in meetings])
-        print(meetings_display) # testing purpose
+        logging.info('Check job initiated.')
+        meetings = self.load_meetings(yaml_dir)
+
         # now convert meetings to a list of ical
+        for m in meetings:
+            m.write_ical()
+        logging.info('Wrote %d meetings to iCal' % (len(meetings)))
+        logging.info('Check job finished.')
 
     def execute_gate(self):
         pass
@@ -23,17 +34,18 @@ class MeetingJobs:
     def execute_post(self):
         pass
 
-    def create_meetings(self, yaml_dir):
+    def load_meetings(self, yaml_dir):
         os.chdir(yaml_dir)
-        meetings_yaml = [yaml.load(open(f, 'r')) for f in os.listdir() if os.path.isfile(f) and ".yaml" in f]
-        meetings = [Meeting(y) for y in meetings_yaml]
+        meetings_yaml = [f for f in os.listdir() if os.path.isfile(f) and f.endswith('yaml')]
+        meetings = [Meeting(yaml.load(open(f, 'r')), f) for f in meetings_yaml]
+        logging.info('Loaded %d meetings from YAML' % (len(meetings)))
         return meetings
 
 def pprint_yaml():
     """For now, this is a simple script to import all the yaml files and pretty print it."""
 
     # change the current directory to the meetings directory where all the yaml files are located
-    os.chdir('../meetings/')
+    os.chdir(yaml_dir)
 
     # get a list of all the yaml files
     meetings = [yaml.load(open(f, 'r')) for f in os.listdir() if os.path.isfile(f) and ".yaml" in f]
@@ -42,6 +54,5 @@ def pprint_yaml():
         print(yaml.dump(m))
 
 # entry point
-#pprint_yaml()
 jobs = MeetingJobs()
 jobs.execute_check()
